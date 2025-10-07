13be29240145416f272fcdd25cf34eab22c2d805
lyminhthai2009 <thailyminh199@gmail.com>
2025-10-07 23:24:07 +0700

    Fix(attempt 3): Set correct Particle names for Paper 1.20.1

--- NMS/v1_21/src/main/java/github/nighter/smartspawner/v1_21/ParticleInitializer.java
+++ NMS/v1_21/src/main/java/github/nighter/smartspawner/v1_21/ParticleInitializer.java
@@ -5,8 +5,8 @@ import org.bukkit.Particle;
 
 public class ParticleInitializer {
     public static void init() {
-        // Sửa tên particle cho tương thích với 1.20.1
-        ParticleWrapper.VILLAGER_HAPPY = Particle.HAPPY_VILLAGER; 
-        ParticleWrapper.SPELL_WITCH = Particle.WITCH;
+        // Tên đúng cho Paper 1.20.1
+        ParticleWrapper.VILLAGER_HAPPY = Particle.VILLAGER_HAPPY; 
+        ParticleWrapper.SPELL_WITCH = Particle.SPELL_WITCH;
     }
 }
