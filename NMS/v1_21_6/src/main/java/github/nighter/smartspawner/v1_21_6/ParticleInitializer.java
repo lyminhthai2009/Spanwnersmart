2fd0675f650ff4752c870bace1b0aab972062234
lyminhthai2009 <thailyminh199@gmail.com>
2025-10-07 23:07:30 +0700

    Fix: Correct Particle names for 1.20.1 compatibility

--- NMS/v1_21_6/src/main/java/github/nighter/smartspawner/v1_21_6/ParticleInitializer.java
+++ NMS/v1_21_6/src/main/java/github/nighter/smartspawner/v1_21_6/ParticleInitializer.java
@@ -1,11 +1,9 @@
 package github.nighter.smartspawner.v1_21_6;
-
 import github.nighter.smartspawner.nms.ParticleWrapper;
 import org.bukkit.Particle;
-
 public class ParticleInitializer {
     public static void init() {
-        ParticleWrapper.VILLAGER_HAPPY = Particle.HAPPY_VILLAGER;
-        ParticleWrapper.SPELL_WITCH = Particle.WITCH;
+        ParticleWrapper.VILLAGER_HAPPY = Particle.VILLAGER_HAPPY;
+        ParticleWrapper.SPELL_WITCH = Particle.SPELL_WITCH;
     }
 }
